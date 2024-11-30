32fdf387dd2fa61e3bd40863f44966938569fd51
joshsurin <joshua.surin@gmail.com>
2024-11-30 06:40:08 +0000

    rm lock

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 	    inputs.nixpkgs.follows = "nixpkgs";
 	};
     };
-    outputs = { nixpkgs, home-manager, ... }:
+    outputs = { nixpkgs, home-manager, nixos-wsl, ... }:
         let
             system = "x86_64-linux";
             pkgs = import nixpkgs { inherit system; };
