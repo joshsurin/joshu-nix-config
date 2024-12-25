c57a02f0cf9bb4a3d9d561acfa02568b665b7876
joshsurin <joshua.surin@gmail.com>
2024-12-25 11:38:20 +1100

    fzf native

--- nvim-flake/lib/buildPlugin.nix
+++ nvim-flake/lib/buildPlugin.nix
@@ -9,48 +9,16 @@ with lib;
 with builtins; let
   inherit (prev.vimUtils) buildVimPlugin;
 
-  treeSitterPlug = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    p.tree-sitter-c
-    p.tree-sitter-nix
-    p.tree-sitter-python
-    p.tree-sitter-rust
-    p.tree-sitter-markdown
-    p.tree-sitter-comment
-    p.tree-sitter-toml
-    p.tree-sitter-make
-    p.tree-sitter-tsx
-    p.tree-sitter-typescript
-    p.tree-sitter-html
-    p.tree-sitter-javascript
-    p.tree-sitter-css
-    p.tree-sitter-graphql
-    p.tree-sitter-json
-    p.tree-sitter-go
-    p.tree-sitter-lua
-    p.tree-sitter-markdown-inline
-    p.tree-sitter-bash
-    p.tree-sitter-regex
-    p.tree-sitter-vim
-    p.tree-sitter-query
-    p.tree-sitter-ocaml
-    p.tree-sitter-ocaml-interface
-    p.tree-sitter-svelte
-  ]);
-
   buildPlug = name:
     buildVimPlugin {
       pname = name;
       version = "master";
       src = builtins.getAttr name inputs;
-      # Add the preFixup for telescope-fzf-native
-      preFixup = if name == "telescope-fzf-native" then ''
-        echo "Building telescope-fzf-native..."
-        make
-      '' else "";
     };
 
   vimPlugins = {
-    inherit treeSitterPlug;
+    treeSitterPlug = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+    telescope-fzf-native = pkgs.vimPlugins.telescope-fzf-native-nvim;
   };
 in {
   neovimPlugins = let
