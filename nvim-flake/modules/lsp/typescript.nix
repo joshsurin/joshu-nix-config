0e2962d92d6dd7861a6c5275edc4727f36ade7aa
joshu <joshua.surin@gmail.com>
2025-01-24 23:43:08 +1100

    a

--- nvim-flake/modules/lsp/typescript.nix
+++ nvim-flake/modules/lsp/typescript.nix
@@ -6,6 +6,7 @@ lspconfig.tsserver.setup{
   on_attach = function(client, bufnr)
     attach_keymaps(client, bufnr)
   end,
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "svelte.typescript", "svelte" },
   cmd = { "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server", "--stdio" },
   handlers = handlers
 }
@@ -16,6 +17,9 @@ lspconfig.svelte.setup{
     attach_keymaps(client, bufnr)
   end,
   cmd = { "${pkgs.nodePackages.svelte-language-server}/bin/svelteserver", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "svelte.typescript", "svelte" },
   handlers = handlers,
+
+  vim.filetype.add({ extension = { ['svelte.ts'] = 'svelte.typescript' } })
 }
 ''
